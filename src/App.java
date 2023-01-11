39edfc364e7885735a6bbab36d5d64c4c63697d9
Jose Pedraza <jpedraza066@gmail.com>
2023-01-11 01:09:36 -0600

    Transition

--- src/App.java
+++ src/App.java
@@ -16,6 +16,6 @@ while loop
 
 public class App {  
   public static void main(String args[]) throws Exception {  
-    Window w = new Window();    
+    new Window().setVisible(true);
   }  
 }  
--- src/Window.java
+++ src/Window.java
@@ -4,10 +4,15 @@ import javax.sound.sampled.*;
 import java.io.File;
 
 public class Window extends JFrame implements ActionListener {  
+  private JLabel main_menu = new JLabel();
   private JButton start;
   private Clip clip = AudioSystem.getClip();
   
   public Window() throws Exception {
+    startGame();
+  }
+
+  public void startGame() throws Exception {
     setSize(800, 600);  
     setLayout(null);
     setResizable(false);
@@ -23,10 +28,6 @@ public class Window extends JFrame implements ActionListener {
     start.setBounds(240,327,320,100);
     start.setBorder(BorderFactory.createEmptyBorder());
 
-    // Adds to Frame
-    getContentPane().add(start_menu);
-    getContentPane().add(start);
-
     start.addMouseListener(new MouseAdapter() {
       public void mouseEntered(MouseEvent evt) {
          start.setIcon(new ImageIcon("src/img/start_h.png"));
@@ -38,16 +39,20 @@ public class Window extends JFrame implements ActionListener {
 
     start.addActionListener(this);
   
-    AudioInputStream main_menu = getAudioFile("main_menu.wav");
-    clip.open(main_menu);      
+    AudioInputStream main_menu_wav = getAudioFile("main_menu.wav");
+    clip.open(main_menu_wav);      
     clip.loop(Clip.LOOP_CONTINUOUSLY);
     clip.start();
-    
-    setVisible(true);  
+        
+    // Adds Label to Frame
+    main_menu.setBounds(getBounds());
+    main_menu.add(start);
+    main_menu.add(start_menu);
+    add(main_menu);
   }
 
-  public AudioInputStream getAudioFile(String path) throws Exception {
-    return AudioSystem.getAudioInputStream(new File("src/aud/" + path).getAbsoluteFile());
+  public AudioInputStream getAudioFile(String filename) throws Exception {
+    return AudioSystem.getAudioInputStream(new File("src/aud/" + filename).getAbsoluteFile());
   }
 
   @Override
@@ -62,10 +67,20 @@ public class Window extends JFrame implements ActionListener {
         clip.start();
 
         Thread.sleep(500);
-      } catch (Exception e1) {}
 
-      
+        ImageIcon transition_ImageIcon = new ImageIcon("src/img/transition.png");
+        JLabel transition = new JLabel();
+        transition.setIcon(transition_ImageIcon);
+        transition.setBounds(0,600,800,1800);
+        add(transition);
+        
+        for(int i = 600; i > -1200; i--) {
+          transition.setBounds(0, i, 800, 1800);
+          Thread.sleep(1);
+        }
+
+      } catch (Exception e1) {}
+ 
     }
-    
   }
 }  
